1bae645095815fcd23a0c0b2c53b4dc433d29773
Arnaud 🎈 <arnaud@streamlit.io>
2022-06-23 18:52:19 +0200

    Enable secrets

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,8 +2,8 @@ import os
 import shutil
 
 REMOVE_PATHS = [
-    '{% if not cookiecutter.app_uses_secrets|int %} .streamlit/secrets.toml {% endif %}',
-    '{% if not cookiecutter.app_is_multi_page|int %} pages {% endif %}',
+    "{% if not cookiecutter.app_uses_secrets|int %} .streamlit/secrets.toml {% endif %}",
+    "{% if not cookiecutter.app_is_multi_page|int %} pages {% endif %}",
 ]
 
 for path in REMOVE_PATHS:
@@ -13,3 +13,11 @@ for path in REMOVE_PATHS:
             shutil.rmtree(path, ignore_errors=True)
         else:
             os.unlink(path)
+
+# Add Streamlit secrets to .gitignore
+with open(".gitignore", "a") as gitignore:
+    gitignore.write("\n")
+    gitignore.write("\n")
+    gitignore.write("# Add Streamlit secrets to .gitignore")
+    gitignore.write("\n")
+    gitignore.write(".streamlit/secrets.toml")
